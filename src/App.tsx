5432e2eea263ea538755953146b417b3a2a379ea
Dhruv Dhaduk <dhadukd44@gmail.com>
2025-08-02 13:27:07 +0530

    scaffold

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from './components/ui/button';
+
+function App() {
+    return (
+        <div className="w-screen h-screen flex items-center justify-center">
+            <Button>Open</Button>
+        </div>
+    );
+}
+
+export default App;
